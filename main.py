5a446ade366906ced2386a2f86a236e3f2bd63b8
Ana Luiza <analuizasantosnascimento4@gmail.com>
2025-10-03 10:36:47 -0300

    criação de tabela

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import sqlite3
 
-conexao = sqlite3 .connect("biblioteca.db")
+def criando_tabela():
+    try:
+        conexao = sqlite3 .connect("biblioteca.db")
+        cursor = conexao.cursor()
 
-cursor = conexao.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS LIVROS (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            autor TEX NOT NULL,
+            ano INTEGER,
+            disponivel TEXT               
+    )                            
+ """)
+    except Exception as erro:
+        #Caso ocorra algum erro no banco
+        print(f"erro ao tentar criar tabela  {erro}")
+    finally:
+        #Sempre fechar a conexão
+        if conexao:
+            conexao.close()
+
+criando_tabela()            
